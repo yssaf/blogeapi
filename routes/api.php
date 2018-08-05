748c96963c6d1bfc88d7d8069718bca359856fe8
yousuf.hajjar@gmail.com <eng.yusuf.hajjar@gmail.com>
2018-08-06 01:28:52 +0300

    model product review -a and route api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::apiResource('product', 'ProductController');
+
+Route::group(['prefix' => 'products'], function(){
+
+    Route::apiResource('/{product}/reviews', 'ReviewController');
+
+});
