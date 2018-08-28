732321575b16df1543f9d9741ad7f6f54a31b6b4
yousuf.hajjar@gmail.com <eng.yusuf.hajjar@gmail.com>
2018-08-28 12:46:34 +0300

    product create update

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -10,6 +10,10 @@ class Product extends Model
 {
     //
 
+    protected $fillable = [
+      'name','detail','stock','price','discount'
+    ];
+
     public function reviews(){
 
         return $this->hasMany(Review::class);
