d58bb6888cbe1e338d6da04885f1f5789f1dd016
yousuf.hajjar@gmail.com <eng.yusuf.hajjar@gmail.com>
2018-08-29 12:32:16 +0300

    product destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -133,5 +133,8 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         //
+        $product->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
